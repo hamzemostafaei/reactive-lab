da780dc3398e22e741775d4be68522029efa2a86
Hamze Mostafaei <67759433+hamzemostafaei@users.noreply.github.com>
2022-10-13 01:38:18 +0330

    Add files via upload

---
+++ reactive-lab-echo/src/main/java/com/hamze/reactivelabecho/ReactiveLabEchoApplication.java
@@ -0,0 +1,13 @@
+package com.hamze.reactivelabecho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveLabEchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveLabEchoApplication.class, args);
+	}
+
+}
---
+++ reactive-lab-echo/src/test/java/com/hamze/reactivelabecho/ReactiveLabEchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamze.reactivelabecho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveLabEchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
