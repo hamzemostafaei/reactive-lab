3c605d885f5d2d89c94610bf7e64f280aa7b6bbe
hamze <hamze.mostafaei@gmail.com>
2025-04-13 11:24:34 +0330

    Implemented web-flux web security

---
+++ reactive-lab-echo/src/main/java/com/hamze/reactivelabecho/controller/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.hamze.reactivelabecho.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .cors(ServerHttpSecurity.CorsSpec::disable)
+                .authorizeExchange(requests -> requests.anyExchange().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+
+        return new MapReactiveUserDetailsService(
+                User
+                        .withUsername("hamze")
+                        .password(passwordEncoder().encode("123456"))
+                        .roles("USER")
+                        .build()
+        );
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
